dc928b782a74cd2a006bf57a94b99225f0f10f03
sasa309 <plearn309@gmail.com>
2023-12-17 15:18:09 +0200

    this is code c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a line with printf function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
